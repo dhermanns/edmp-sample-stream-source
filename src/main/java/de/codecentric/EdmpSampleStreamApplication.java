2cb7718be88cc98be7eaa558ff4095b4da68c3c5
tobiasflohre <tobias.flohre@codecentric.de>
2016-03-13 08:53:15 +0100

    unused imports

--- src/main/java/de/codecentric/EdmpSampleStreamApplication.java
+++ src/main/java/de/codecentric/EdmpSampleStreamApplication.java
@@ -1,9 +1,7 @@
 package de.codecentric;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
